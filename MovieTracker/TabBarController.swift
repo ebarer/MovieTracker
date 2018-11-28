cdbe3993e79cbc5ac31bf1d686f166e612e6c963
Elliot Barer <ebarer@mac.com>
2018-11-28 00:46:45 -0800

    - TabBar bug fixes and clean up

--- MovieTracker/TabBarController.swift
+++ MovieTracker/TabBarController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.tintColor = UIColor.accent
@@ -21,25 +22,33 @@ class TabBarController: UITabBarController {
 // MARK: - UITabBarControllerDelegate
 
 extension TabBarController: UITabBarControllerDelegate {
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        guard let navVC = self.selectedViewController as? UINavigationController else {
-            print("Error: invalid nav VC")
-            return
-        }
-
-        let vcStack = navVC.viewControllers
-        if vcStack.count > 2 {
-            // Ensure view is scrolled to top
-            (vcStack[1] as? MovieDetailViewController)?.tableView.setContentOffset(.zero, animated: false)
-            navVC.popToViewController(vcStack[1], animated: true)
-        } else {
-            navVC.popToRootViewController(animated: true)
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        currentIndex = (self.viewControllers as NSArray?)?.index(of: viewController) ?? -1
+        print(selectedIndex, currentIndex)
+        if tabBarController.selectedViewController == viewController && currentIndex == 0 {
+            if let navVC = viewController as? UINavigationController {
+                let vcStack = navVC.viewControllers
+                // Deep traversal through multiple movies/people controllers
+                if vcStack.count > 2 {
+                    // Ensure view is scrolled to top
+                    (vcStack[1] as? MovieDetailViewController)?.tableView.setContentOffset(.zero, animated: false)
+                    navVC.popToViewController(vcStack[1], animated: true)
+                }
+                // First movie controller
+                else if vcStack.count == 2 {
+                    navVC.popToRootViewController(animated: true)
+                }
+                // Feature collection controller
+                else {
+                    // Scroll collection view to top
+                    let offset = CGPoint(x: 0, y: -1 * vcStack[0].view.safeAreaInsets.top)
+                    (vcStack[0] as? FeaturedViewController)?.collectionView.setContentOffset(offset, animated: true)
+                }
+                
+                return false
+            }
         }
         
-        print(item)
-    }
-    
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        return false
+        return true
     }
 }
